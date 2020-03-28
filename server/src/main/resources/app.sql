2deb48f2e21ab031fee3b1fe5f7c08a785a2a808
Xu Yunpeng <xu.yunpeng@flexiht.com>
2020-03-28 15:25:52 +0800

    update sql to add demo data

--- server/src/main/resources/app.sql
+++ server/src/main/resources/app.sql
@@ -1,10 +1,60 @@
-CREATE DATABASE `app` DEFAULT CHARACTER SET utf8;
+/*
+SQLyog Ultimate v12.3.1 (64 bit)
+MySQL - 8.0.19 : Database - app
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`app` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 USE `app`;
-SET FOREIGN_KEY_CHECKS=0;
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`name`) values 
+(1,'admin'),
+(2,'client');
+
+/*Table structure for table `role_user` */
+
+DROP TABLE IF EXISTS `role_user`;
+
+CREATE TABLE `role_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int DEFAULT '2',
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `role_user_ibfk_2` (`user_id`),
+  CONSTRAINT `role_user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `role_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `role_user` */
+
+/*Table structure for table `user` */
 
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(64) DEFAULT NULL,
   `nickname` varchar(64) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
@@ -12,26 +62,17 @@ CREATE TABLE `user` (
   `email` varchar(64) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*Data for the table `user` */
 
-INSERT INTO `role` VALUES ('1', 'admin');
-INSERT INTO `role` VALUES ('2', 'client');
+insert  into `user`(`id`,`username`,`nickname`,`password`,`enabled`,`email`,`create_time`) values 
+(2,'Test1','test1','123456',1,'test1@test.com','2020-03-28 14:03:59'),
+(3,'Test2','Test2','123456',1,'test2@test.com','2020-03-28 14:04:04'),
+(9,'test9','sdfsdf',NULL,1,'sdfdsf@sdf.com',NULL),
+(11,'test11','tedwef',NULL,1,'sds@fhwoe.com',NULL);
 
-DROP TABLE IF EXISTS `roles_user`;
-CREATE TABLE `role_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_id` int(11) DEFAULT '2',
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `role_id` (`role_id`),
-  KEY `role_user_ibfk_2` (`user_id`),
-  CONSTRAINT `role_user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
-  CONSTRAINT `role_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
