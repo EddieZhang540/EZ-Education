4446c03470909ff3eb85b5fde41bf952251a82a7
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2020-06-08 10:44:57 -0400

    updated homepage

--- client/js/components/Home.js
+++ client/js/components/Home.js
@@ -6,7 +6,10 @@ const Home = {
                 <!-- captions -->
                 <div class="caption text-center">
                     <h1>WELCOME TO EZ EDUCATION</h1>
-                    <h3>------==------</h3>
+                    <h3>YOUR PERSONAL JOURNEY STARTS HERE</h3>
+                    <a href="#">
+                        <img src="img/down-arrow.png" width="75" height="80" alt="">
+                    </a>
                 </div>
             </div>
         </div>
