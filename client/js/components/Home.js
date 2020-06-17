1a7a6b72ab4a1710f935b1c5293d3fc7dac689af
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2020-06-16 21:30:12 -0400

    update html

--- client/js/components/Home.js
+++ client/js/components/Home.js
@@ -1,17 +1,6 @@
 const Home = {
     template: `
         <div>
-            <!-- landing page -->
-            <div class="landing">
-                <!-- captions -->
-                <div class="caption text-center">
-                    <h1>WELCOME TO EZ EDUCATION</h1>
-                    <h3>YOUR PERSONAL JOURNEY STARTS HERE</h3>
-                    <a href="#">
-                        <img src="img/down-arrow.png" width="75" height="80" alt="">
-                    </a>
-                </div>
-            </div>
         </div>
     `
 }
