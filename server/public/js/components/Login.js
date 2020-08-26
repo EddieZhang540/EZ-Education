5c0b737f2e9ea5aa7f48df482b2607bc683c77e2
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2020-08-26 14:58:32 -0400

    fixed login validation

--- server/public/js/components/Login.js
+++ server/public/js/components/Login.js
@@ -1,12 +1,43 @@
 const Login = {
     data: function(){
         return {
-            user : {}
+            user : {},
+            errors : {}
         };
     },
     methods: {
         login: function(){
+            if(!this.validateForm()) return;
             alert('TODO: login');
+        },
+        validateForm: function(){
+            this.errors = {};
+            this.validateEmail();
+            this.validatePassword();
+            for( let key in this.errors){
+                return false;
+            }
+            return true;
+        },
+        validateEmail: function(){
+            delete this.errors.email;
+            if(!this.user.email){
+                this.errors.email='Please enter the email';
+            }
+        },
+        validatePassword: function(){
+            delete this.errors.password;
+            if(!this.user.password){
+                this.errors.password='Please enter the password';
+            }
+        }
+    },
+    watch : {
+        "user.email" : function(){
+            this.validateEmail();
+        },
+        "user.password" : function(){
+            this.validatePassword();
         }
     }
 }
