1dc318cbe2802e6f0413be5995d281cedcfa3994
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2020-11-07 18:20:49 -0500

    oe

--- server/public/js/components/Home.js
+++ server/public/js/components/Home.js
@@ -18,10 +18,10 @@ const Home = {
                     <div class="collapse navbar-collapse" id="navbarCollapsible">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="#home" v-smooth-scroll="{ duration: 600 }">Home</a>
+                                <a class="nav-link" href="#home" v-smooth-scroll="{ duration: 1000 }">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#about" v-smooth-scroll="{ duration: 600, offset: -180 }">About</a>
+                                <a class="nav-link" href="#about" v-smooth-scroll="{ duration: 1000, offset: -155 }">About</a>
                             </li>
                             <!--
                             <li class="nav-item">
@@ -29,10 +29,10 @@ const Home = {
                             </li>
                             -->
                             <li class="nav-item">
-                                <a class="nav-link" href="#team" v-smooth-scroll="{ duration: 600, offset: -105 }">The Team</a>
+                                <a class="nav-link" href="#team" v-smooth-scroll="{ duration: 1000, offset: -105 }">The Team</a>
                             </li>
                             <li class="nav-item mr-5">
-                                <a class="nav-link" href="#contact" v-smooth-scroll="{ duration: 600, offset: -105 }">Contact Us</a>
+                                <a class="nav-link" href="#contact" v-smooth-scroll="{ duration: 1000, offset: -105 }">Contact Us</a>
                             </li>
                             <li class="nav-item">
                                 <a class="btn btn-outline-primary" v-on:click="signup">Sign Up</a>
@@ -52,7 +52,7 @@ const Home = {
                     <div class="caption text-center">
                         <h1>WELCOME TO EZ EDUCATION</h1>
                         <h3>YOUR PERSONAL JOURNEY STARTS HERE</h3>
-                        <a href="#about" v-smooth-scroll="{ duration: 600, offset: -180 }">
+                        <a href="#about" v-smooth-scroll="{ duration: 1000, offset: -155 }">
                             <img src="img/down-arrow.png" width="75" height="80" alt="">
                         </a>
                     </div>
@@ -67,49 +67,58 @@ const Home = {
                         <h1>ABOUT</h1>
                         <hr class="short">
                         <p>
-                        EZ Education is a not-for-profit organization based in the Greater Toronto Area. Our mission is to provide a platform that connects people learning English as a foreign language with volunteer teachers who are fluent in English. 
-                        At EZ Education, we aim to to offer a more relaxed and flexible learning environment than a traditional classroom. The teachers and students are free to decide which subjects to cover and how the lessons will be delivered. Through
-                        one-on-one lessons and discussions, EZ Education promotes a broader understanding of the English language and facilitates strong and meaningful relationships between students and teachers.
+                        EZ Education is a student-run initiative based in the Greater Toronto Area. We operate as a not-for-profit organization partnered with the Rotary Club of Mississauga Streetsville.
+                        </p>
+                        <p>
+                        We provide a platform that connects people learning English as a foreign language with volunteer teachers who are fluent in English.
+                        </p>
+                        <p>
+                        Through one-on-one classes and discussions, EZ Education promotes a richer understanding of English and facilitates meaningful relationships between students and teachers.
                         </p>
                     </div>
                 </div>
 
-                <div id="our-mission" class="row">
+                <div id="mission" class="row">
                     <div class="col-7 text-center">
-                        <h1>Our Mission</h1>
-                        <p>Our mission is to break down lingual and cultural barriers between anyone, from anywhere. We completely understand what it's like to be a complete stranger in a new country, having experienced the exact same thing ourselves, which is why we work to ease the transition for as many newcomers as possible. We do that by building a motivated and diverse team and making sure that everyone, both students and teachers, feel not just comfortable, but passionate about global communication, interaction and support.</p>
-                    </div>
+                        <h2>OUR MISSION</h2>
+                        <p>
+                            At EZ Education, we strive to help break down the language and cultural barriers that stand in the way of immigrants to English-speaking countries. We understand what it's like to be a stranger in a foriegn land,
+                            having experienced similar circumstances ourselves, which is why we work to ease the transition for as many newcomers as possible. We do so by building a motivated and diverse team and ensuring that our students and
+                            teachers feel not only comfortable, but also excited about global communication and interaction.
+                        </p>
+                    </div>  
                 </div>
 
                 <div id="for-student" class="row">
-                    <div class="col-8 text-center">
+                    <div class="col-9 text-center">
                         <div class="row">
                             <h2 class="col">What we offer</h2>
                         </div>
                         <div class="row">
-                            <div class="col">
-                                <div class="card">
+                            <div class="col-sm">
+                                <div class="card shadow">
                                     <div class="card-body">
                                         <div class="icon" style="background: #fceef3;"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
-                                        <h4>A</h4>
+                                        <h4>Flexibility</h4>
+                                        <p class="card-text">We aim to offer a more relaxed and flexible learning environment than a traditional classroom. The teachers and students are free to decide which subjects to cover and how each lesson will be delivered to cater to the diverse range of learners.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm">
+                                <div class="card shadow">
+                                    <div class="card-body">
+                                        <div class="icon" style="background: #fceef3;"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
+                                        <h4>Authenticity</h4>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="card">
+                            <div class="col-sm">
+                                <div class="card shadow">
                                     <div class="card-body">
                                         <div class="icon" style="background: #fceef3;"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
-                                        <h4>B</h4>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="icon" style="background: #fceef3;"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
-                                        <h4>C</h4>
+                                        <h4>Accessibility</h4>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                     </div>
                                 </div>
