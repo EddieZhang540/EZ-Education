c3a7635b00dd08a48ab339b01f2372e21119de2b
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2021-01-05 23:12:20 -0500

    update front page

--- server/public/js/components/Home.js
+++ server/public/js/components/Home.js
@@ -4,7 +4,7 @@ const Home = {
             <header class="main-header">
                 <nav class="navbar fixed-top navbar-expand-lg shadow">
                     <router-link class="navbar-brand" to="/">
-                        <img src="img/ez education logo copy.png" height="80" alt="EZ Education">
+                        <img src="img/ez education logo copy.png" height="80" alt="EZ Education" />
                         <!-- <img src="img/brand.png" width="164" height="80" alt="EZ Education"> -->
                     </router-link>
 
@@ -53,28 +53,20 @@ const Home = {
                         <h1>WELCOME TO EZ EDUCATION</h1>
                         <h3>ESL LEARNING MADE EASY</h3>
                         <a href="#about" v-smooth-scroll="{ duration: 1000, offset: -105 }">
-                            <img src="img/down-arrow.png" width="75" height="80" alt="">
+                            <img src="img/down-arrow.png" width="75" height="80" alt="" />
                         </a>
                     </div>
                 </div>
 
                 <!-- about -->
                 <div id="about" class="row">
-                    <div class="col-1"></div>
-                    <div class="col-4">
-                    </div>
-                    <div class="col-6">
+                    <div class="col-lg-3"></div>
+                    <div class="col-lg-6">
                         <h1>ABOUT</h1>
-                        <hr class="short">
-                        <p>
-                        EZ Education is a student-run initiative based in the Greater Toronto Area, Canada. We operate as a not-for-profit organization partnered with the Rotary Club of Mississauga Streetsville.
-                        </p>
-                        <p>
-                        We provide a platform that connects people learning English as a foreign language with volunteer teachers who are fluent in English.
-                        </p>
-                        <p>
-                        Through one-on-one classes and discussions, EZ Education promotes a richer understanding of English and facilitates meaningful relationships between students and teachers.
-                        </p>
+                        <hr class="short" />
+                        <p>EZ Education is a student-run initiative based in the Greater Toronto Area, Canada. We operate as a not-for-profit organization partnered with the Rotary Club of Mississauga Streetsville.</p>
+                        <p>We provide a platform that connects people learning English as a foreign language with volunteer teachers who are fluent in English.</p>
+                        <p>Through one-on-one classes and discussions, EZ Education promotes a richer understanding of English and facilitates meaningful relationships between students and teachers.</p>
                     </div>
                 </div>
 
@@ -82,11 +74,11 @@ const Home = {
                     <div class="col-8 text-center">
                         <h2>OUR MISSION</h2>
                         <p>
-                            At EZ Education, we strive to help break down the language and cultural barriers that stand in the way of immigrants to English-speaking countries. We understand what it's like to be a stranger in a foriegn land,
-                            having experienced similar circumstances ourselves, which is why we work to ease the transition for as many newcomers as possible. We do so by building a motivated and diverse team and ensuring that our students and
-                            teachers feel not only comfortable, but also excited about global communication and interaction.
+                            At EZ Education, we strive to break down the cultural and linguistic barriers for immigrants to English-speaking countries. We understand what it's like to be a stranger in a foreign land, having experienced similar circumstances
+                            ourselves, which is why we work to ease the transition for as many newcomers as possible. We do so by building a motivated and diverse team. Through our curriculum and training, we aim to ensure that our students and teachers feel
+                            not only comfortable, but also excited about global communication and interaction.
                         </p>
-                    </div>  
+                    </div>
                 </div>
 
                 <div id="for-student" class="row">
@@ -95,39 +87,44 @@ const Home = {
                             <h2 class="col">FOR STUDENTS, WE OFFER...</h2>
                         </div>
                         <div class="row">
-                            <div class="col-xl">
+                            <div class="col-lg">
                                 <div class="card shadow">
                                     <div class="card-body">
-                                        <div style="background: #fceef3"><i class="fas fa-drafting-compass" style="color: #ff689b"></i></div>
+                                        <div class="fa-stack">
+                                            <i class="fas fa-drafting-compass" style="color: #ff689b"></i>
+                                        </div>
                                         <h4>Flexibility</h4>
                                         <p class="card-text">
-                                            Recognizing that different individuals vary in background, proficiency and learning styles, EZ Education aims to offer a more relaxed and flexible learning environment than a traditional classroom.
-                                            Students are free to decide which subjects to cover and discuss how lesson will be delivered with their teachers.
+                                            Recognizing that different individuals vary in background, proficiency, and learning styles, EZ Education proposes a more relaxed and flexible learning environment than a traditional classroom. Students are allowed
+                                            and encouraged to discuss the delivery and content of the lessons with their teachers.
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl">
+                            <div class="col-lg">
                                 <div class="card shadow">
                                     <div class="card-body">
-                                        <div style="background: #fff7e1"><i class="fas fa-balance-scale" style="color: #ffdc7b"></i></div>
+                                        <div class="fa-stack">
+                                            <i class="fas fa-balance-scale" style="color: #ffdc7b"></i>
+                                        </div>
                                         <h4>Authenticity</h4>
                                         <p class="card-text">
-                                            Practical application is often an overlooked element in many ESL programs. Immigrant students could learn to read and write in English very well, only to find out that they have trouble understanding
-                                            native speakers. EZ Education recruits fluent English speakers in the GTA as volunteers teachers. By learning and interacting with their teachers, students will grasp the implicit cultural concepts and
-                                            idioms much more effectively than a regular English course.
+                                            ESL students could often learn to read and write in English very well, but still struggle to understand native speakers. EZ Education recruits fluent English speakers in the GTA as volunteer teachers. They will
+                                            help students grasp the implicit cultural concepts and idioms much more effectively than a regular English course.
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl">
+                            <div class="col-lg">
                                 <div class="card shadow">
                                     <div class="card-body">
-                                        <div style="background: #dfd9ff"><i class="fas fa-wifi" style="color: #857aff"></i></div>
+                                        <div class="fa-stack">
+                                            <i class="fas fa-wifi" style="color: #857aff"></i>
+                                        </div>
                                         <h4>Accessibility</h4>
                                         <p class="card-text">
-                                            EZ Education is located in the GTA. However, we do not limit student membership to only new immigrants in the GTA. By delivering lessons through online platforms such as Zoom, Wechat, and WhatsApp, EZ
-                                            Education welcomes ESL students from any part of the world to have the opportunity to learn from a native English speaker, for free.
+                                            EZ Education operates in the GTA. However, we do not limit student membership to only new immigrants in the GTA. By delivering lessons through platforms like Zoom, WeChat, and WhatsApp, EZ Education welcomes ESL
+                                            students from any part of the world to have the opportunity to learn from a native English speaker, for free.
                                         </p>
                                     </div>
                                 </div>
@@ -144,26 +141,18 @@ const Home = {
                 </div>
 
                 <!-- contact -->
-                <div id="contact" class="row">
-
-                </div>
+                <div id="contact" class="row"></div>
             </main>
 
             <footer>
                 <div class="container-fluid">
                     <div class="row text-center">
-                        <div class="col">
-                            column 1
-                        </div>
-                        <div class="col">
-                            column 2
-                        </div>
-                        <div class="col">
-                            column 3
-                        </div>
+                        <div class="col">column 1</div>
+                        <div class="col">column 2</div>
+                        <div class="col">column 3</div>
 
                         <div class="col-12">
-                            <hr>
+                            <hr />
                             <h6>&copy; ez-edu</h6>
                         </div>
                     </div>
